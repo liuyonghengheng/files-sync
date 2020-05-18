a64e10922766d9ba3cea3a1a1dd2a16bbc670103
yongheng.liu <yongheng.liu@kyligence.io>
2020-05-18 17:53:07 +0800

    add filter abstract class

---
+++ files_sync/files_filters/files_filter.py
@@ -0,0 +1,12 @@
+
+class FilesFilter:  # pylint: disable=too-few-public-methods
+    """Class to keep module of Authentication API  """
+    def __init__(self):
+        pass
+
+    def event_filter(self, event):
+        pass
+
+    @staticmethod
+    def filter_paths(self, paths):
+        pass
